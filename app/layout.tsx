b7dadac56152784f764eb8b977e270c71d55a067
GuCarrara <gustavo.carrara93@gmail.com>
2026-04-03 00:14:58 -0300

    fix: remover Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/auth";
 import ClientLayout from "./ClientLayout";
-import Script from "next/script";
 
 export const metadata = {
   title: "EstéticaPro | Sistema para Estéticas Automotivas",
@@ -19,13 +18,8 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   return (
     <ClientLayout session={session}>
-      <Script
-        id="clarity"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "w56xr03b8c");`,
-        }}
-      />
+      
+      
       {children}
     </ClientLayout>
   );
